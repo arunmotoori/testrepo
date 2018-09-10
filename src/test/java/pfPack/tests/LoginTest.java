59e524f6d4b5d798d7738a7ab11c76ecb9527932
Arun Motoori <arun.motoori@gmail.com>
2018-09-10 12:16:42 +0530

    Update LoginTest.java

--- src/test/java/pfPack/tests/LoginTest.java
+++ src/test/java/pfPack/tests/LoginTest.java
@@ -15,6 +15,7 @@ import pfPack.util.Constants;
 
 public class LoginTest extends BaseTest {
 	
+	//The following is the test to verify the login functionality of the application
 	@Test
 	public void testLogin() {
 		
